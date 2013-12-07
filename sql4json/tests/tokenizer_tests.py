c7e4ae6496393add49a6490692ed701fb22fbc10
Brian Hendriks <brian.hendriks@gmail.com>
2013-12-07 02:27:31 -0800

    Basic string tokenizer

---
+++ sql4json/tests/tokenizer_tests.py
@@ -0,0 +1,80 @@
+import unittest
+
+from s4j.tokenizer import *
+
+class TokenizerTests(unittest.TestCase):
+    def test_tokenizer_init(self):
+        tokenizer = Tokenizer('test')
+        
+        self.assertEqual("test", tokenizer.unparsed_string)
+        self.assertEqual(["test"], tokenizer.tokens)
+
+    def test_add_tokens(self):
+        tokenizer = Tokenizer('')
+
+        tokenizer.unparsed_string = 'this is a test'
+        
+        self.assertEqual( 4, tokenizer._add_token(0, 4) )
+        self.assertEqual( 7, tokenizer._add_token(5, 7) )
+        self.assertEqual( 9, tokenizer._add_token(7, 9) )
+        self.assertEqual( 14, tokenizer._add_token(9, 14) )
+        self.assertEqual( ["this", "is", "a", "test"], tokenizer )
+
+    def test_tokenizer_is_iterable(self):
+        tokenizer = Tokenizer("test 1 2 test1234 a b ab_12")
+
+        expected = ["test","1","2","test1234","a","b","ab_12"]
+
+        for i,token in enumerate(tokenizer):
+            self.assertEqual(expected[i], token)
+
+    def test_chars_classified_properly(self):
+        for i in range(ord(' '), 126):
+            char = chr(i)
+            token_type = Tokenizer.get_token_type(char)
+
+            if char.isdigit() or char.isalpha() or char == '_':
+                self.assertEqual(Tokenizer.TOKEN_TYPES.ALPHANUM, token_type)
+            elif char == ' ' or char == '\t' or char == '\n' or char == '\n':
+                self.assertEqual(Tokenizer.TOKEN_TYPES.WHITESPACE, token_type)
+            elif char == '"' or char == "'":
+                self.assertEqual(Tokenizer.TOKEN_TYPES.QUOTEDSTRING, token_type)
+            elif char == '(':
+                self.assertEqual(Tokenizer.TOKEN_TYPES.PARENOPEN, token_type)
+            elif char == ')':
+                self.assertEqual(Tokenizer.TOKEN_TYPES.PARENCLOSE, token_type)
+            else:
+                self.assertEqual(Tokenizer.TOKEN_TYPES.SYMBOL, token_type)
+
+    def test_len(self):
+        tokenizer = Tokenizer("test 1 2 test1234 a b ab_12")
+        self.assertEqual(7, len(tokenizer))
+
+    def test_tokenize_words(self):
+        tokenizer = Tokenizer("test 1 2 test1234 a b ab_12")
+
+        self.assertEqual(["test","1","2","test1234","a","b","ab_12"], tokenizer)
+
+    def test_tokenize_operators(self):
+        tokenizer = Tokenizer("test!=12&&test!=14 and other_test == 8")
+
+        self.assertEqual(["test","!=","12","&&","test","!=","14","and","other_test","==","8"], tokenizer)
+
+    def test_tokenize_quoted_strings(self):
+        tokenizer = Tokenizer('''test == 'blue ball' or test =="123+123 == 246"''');
+
+        self.assertEqual(["test","==","'blue ball'","or","test","==",'"123+123 == 246"'], tokenizer)
+
+    def test_tokenize_strings_with_parenthesis(self):
+        tokenizer = Tokenizer('''test==1&&(test2==3 or testA=="b") && (x ==3)''');
+
+        self.assertEqual(["test","==","1","&&","(","test2","==","3","or","testA","==",'"b"',")","&&","(","x","==","3",")"], tokenizer)
+
+    def test_tokenize_full_sql_query(self):
+        tokenizer = Tokenizer('''
+            SELECT *
+            FROM data
+            WHERE (id in / or time>0) and test == "value"
+        ''')
+
+        self.assertEqual(["SELECT", "*", "FROM", "data", "WHERE", "(", "id", "in", "/", "or", "time", ">", "0", ")", "and", "test", "==", '"value"'], tokenizer)
---
+++ sql4json/tokenizer.py
@@ -0,0 +1,87 @@
+from enums import CEnum
+
+'''
+Basic class for splitting a string up into tokens.
+'''
+class Tokenizer(object):
+    TOKEN_TYPES = CEnum(("NONE","ALPHANUM","SYMBOL","WHITESPACE","QUOTEDSTRING","PARENOPEN","PARENCLOSE"))
+
+    QUOTE_CHARS = ('"', "'")
+
+    EMPTY_STIRNG_TYPES = (TOKEN_TYPES.WHITESPACE, TOKEN_TYPES.NONE)
+
+    @staticmethod
+    def get_token_type(char):
+        if char.isalpha() or char.isdigit() or char == '_':
+            return Tokenizer.TOKEN_TYPES.ALPHANUM
+        elif char.isspace():
+            return Tokenizer.TOKEN_TYPES.WHITESPACE
+        elif char in Tokenizer.QUOTE_CHARS:
+            return Tokenizer.TOKEN_TYPES.QUOTEDSTRING
+        elif char == '(':
+            return Tokenizer.TOKEN_TYPES.PARENOPEN
+        elif char == ')':
+            return Tokenizer.TOKEN_TYPES.PARENCLOSE
+        else:
+            return Tokenizer.TOKEN_TYPES.SYMBOL
+
+    def __init__(self, str_to_parse):
+        self.unparsed_string = str_to_parse
+        self.tokens = []
+
+        self._parse()
+
+    def _add_token(self, start, end):
+        token = self.unparsed_string[start:end].strip()
+
+        if len(token) > 0:
+            self.tokens.append( token )
+
+        return end
+
+    def _parse(self):
+        start = 0
+        current_quote_char = None
+        current_token_type = Tokenizer.TOKEN_TYPES.NONE
+
+        for i, char in enumerate(self.unparsed_string):
+            if current_quote_char != None:
+                if current_quote_char == char:
+                    start = self._add_token(start, i+1)
+
+                    current_quote_char = None
+                    current_token_type = Tokenizer.TOKEN_TYPES.NONE
+
+            else:
+                token_type = Tokenizer.get_token_type( char )
+
+                if token_type != current_token_type and current_token_type not in Tokenizer.EMPTY_STIRNG_TYPES:
+                    start = self._add_token(start, i)
+
+                if token_type == Tokenizer.TOKEN_TYPES.QUOTEDSTRING:
+                    current_quote_char = char
+
+                current_token_type = token_type
+
+        if start != len(self.unparsed_string):
+            self._add_token(start, len(self.unparsed_string))
+
+    def __getitem__(self,index):
+        return self.tokens[index]
+
+    def __len__(self):
+        return len(self.tokens)
+
+    def __eq__(self, other):
+        if isinstance(other, Tokenizer):
+            return self.tokens == other.tokens
+        elif isinstance(other, list) or isinstance(other, tuple):
+            return self.tokens == other 
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return str(self.tokens)
