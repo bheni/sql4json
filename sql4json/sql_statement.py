5e064c29d6248a694e6c12df41155001ae868064
Brian Hendriks <brian.hendriks@gmail.com>
2013-12-12 00:55:06 -0800

    Refactor sql statement to split query based on key words, but don't fully tokize

--- sql4json/sql_statement.py
+++ sql4json/sql_statement.py
@@ -9,37 +9,81 @@ functions to get the tokens from these sections.
 class SQLStatement(object):
     SECTIONS = CEnum(("SELECT", "FROM", "WHERE"))
     SECTION_NAMES = ("select", "from", "where")
+    SECTION_NAMES_SET = frozenset(SECTION_NAMES)
 
     def __init__(self, sql_statement_string):
-        self.tokenizer = Tokenizer(sql_statement_string)
+        self.sql_statement_string = sql_statement_string.strip()
 
-        self.init_empty_sections()
-        self.init_sections()
+        if not self.sql_statement_string.lower().startswith("select"):
+            raise SQLStatementFormatException('"%s" does not begin with a valid select statement.' % self.sql_statement_string)
 
-    def init_empty_sections(self):
-        self.sections = []
-        for section in SQLStatement.SECTION_NAMES:
-            self.sections.append([])
+        section_start_indices = self.get_start_of_each_section()
+        self.parse_sections( section_start_indices )
 
-    def init_sections(self):
-        current_section = -1
+    def get_start_of_each_section(self):
+        lower = self.sql_statement_string.lower()
+        quote_char = None
+        section_start_indices = [-1,-1,-1]
+        current_chars = []
+        start_index = 0
 
-        for token in self.tokenizer:
-            token_lower = token.lower()
+        for i in range(len(self.sql_statement_string)):
+            lower_char = lower[i]
 
-            if token_lower in SQLStatement.SECTION_NAMES:
-                current_section = SQLStatement.SECTION_NAMES.index(token_lower)
+            if quote_char == lower_char:
+                quote_char = None
+                current_chars = []
+                start_index = i
+            
+            elif lower_char in ('"', "'"):
+                quote_char = lower_char
+                current_chars = [lower_char]
+                start_index = i
+            
+            elif lower_char.isspace():
+                current_word = ''.join(current_chars)
+                if current_word in SQLStatement.SECTION_NAMES_SET:
+                    section_index = SQLStatement.SECTION_NAMES.index(current_word)
+
+                    if section_start_indices[section_index] != -1:
+                        raise SQLStatementFormatException("Multiple %s sections found" % current_word)
+
+                    section_start_indices[section_index] = start_index
+
+                current_chars = []
+                start_index = i
+
             else:
-                if current_section == -1:
-                    raise SQLStatementFormatException('%s found before "select"' % token)
-                else:
-                    self.sections[current_section].append( token )
+                current_chars.append(lower_char)
 
-    def get_select_tokens(self):
+        if quote_char != None:
+            raise SQLStatementFormatException('" mismatch in %s' % self.sql_statement_string)
+        elif ''.join(current_chars) in SQLStatement.SECTION_NAMES_SET:
+            raise SQLStatementFormatException('Invalid %s section' % current_word)
+
+        return section_start_indices
+
+    def parse_sections(self, start_indices):
+        self.sections = [None, None, None]
+        sorted_start_indices = sorted(start_indices)
+
+        for i in range(len(sorted_start_indices)):
+            index = sorted_start_indices[i]
+
+            if index != -1:
+                section_index = start_indices.index(index)
+                index += len(SQLStatement.SECTION_NAMES[section_index]) + 1
+
+                if i < len(sorted_start_indices) - 1:
+                    self.sections[section_index] = self.sql_statement_string[index:sorted_start_indices[i + 1]].strip()
+                else:
+                    self.sections[section_index] = self.sql_statement_string[index:].strip()
+
+    def get_select_section(self):
         return self.sections[SQLStatement.SECTIONS.SELECT]
 
-    def get_from_tokens(self):
+    def get_from_section(self):
         return self.sections[SQLStatement.SECTIONS.FROM]
 
-    def get_where_tokens(self):
+    def get_where_section(self):
         return self.sections[SQLStatement.SECTIONS.WHERE]
--- sql4json/tests/sql_statement_tests.py
+++ sql4json/tests/sql_statement_tests.py
@@ -5,18 +5,32 @@ from sql4json.exceptions import SQLStatementFormatException
 
 class SQLStatementTests(unittest.TestCase):
     def test_parse_empty_statement(self):
-        sql_statement = SQLStatement('')
+        try:
+            sql_statement = SQLStatement('')
+            self.fail()
+        except SQLStatementFormatException:
+            pass
 
-        self.assertEqual([], sql_statement.get_select_tokens())
-        self.assertEqual([], sql_statement.get_from_tokens())
-        self.assertEqual([], sql_statement.get_where_tokens())
+    def test_select(self):
+        sql_statement = SQLStatement("select *")
+        
+        self.assertEqual("*", sql_statement.get_select_section())
+        self.assertEqual(None, sql_statement.get_from_section())
+        self.assertEqual(None, sql_statement.get_where_section())
 
-    def test_parse_sql_statement(self):
+    def test_select_from(self):
+        sql_statement = SQLStatement("select * from some/path")
+        
+        self.assertEqual("*", sql_statement.get_select_section())
+        self.assertEqual("some/path", sql_statement.get_from_section())
+        self.assertEqual(None, sql_statement.get_where_section())
+
+    def test_from_where(self):
         sql_statement = SQLStatement("SELECT id, key3 FROM somewhere WHERE key1==1 or (key2 == 'string value'&&key3>4")
         
-        self.assertEqual(["id", ",", "key3"], sql_statement.get_select_tokens())
-        self.assertEqual(["somewhere"], sql_statement.get_from_tokens())
-        self.assertEqual(["key1", "==", "1", "or", "(", "key2", "==", "'string value'","&&","key3",">","4"], sql_statement.get_where_tokens())
+        self.assertEqual("id, key3", sql_statement.get_select_section())
+        self.assertEqual("somewhere", sql_statement.get_from_section())
+        self.assertEqual("key1==1 or (key2 == 'string value'&&key3>4", sql_statement.get_where_section())
 
     def test_sql_statements_not_starting_with_a_section_name_throw_an_exception(self):
         try:
