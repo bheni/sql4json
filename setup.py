c49c43eaf35dfff84f57272996e4f6ad3d6358ba
Brian Hendriks <brian.hendriks@gmail.com>
2013-12-12 15:14:57 -0800

    setup script and cli
    version 0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sql4json',
+      version='0.1',
+      description='Python SQL library and command line for querying JSON documents',
+      author='Brian Hendriks',
+      url='http://github.com/bheni/sql4json',
+      packages=['sql4json', 'sql4json.boolean_expressions'],
+      scripts=['bin/sql4json']
+     )
